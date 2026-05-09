fcdecf7cd47dde2249d94a2a684c6b5f1739f475
Vinaya <vsvinaya26@gmail.com>
2026-05-09 16:01:18 +0530

    Day 4 File Handling Completed

--- Day3.py
+++ Day3.py
@@ -1,105 +1,105 @@
-# print("Create a list of 10 numbers")
-# numbers = [1,2,3,4,5,6,7,8,9,10]
-# print(numbers)
+print("Create a list of 10 numbers")
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
 
 
-# print("Find sum of list elements")
-# numbers = [1,2,3,4,5]
-# print(sum(numbers))
+print("Find sum of list elements")
+numbers = [1,2,3,4,5]
+print(sum(numbers))
 
 
-# print("Find maximum number")
-# numbers = [10,25,5,40,15]
-# print(max(numbers))
+print("Find maximum number")
+numbers = [10,25,5,40,15]
+print(max(numbers))
 
 
-# print("even numbers from list")
-# numbers = [1,2,3,4,5,6,7,8]
-# for i in numbers:
-#     if i % 2 == 0:
-#         print(i)
+print("even numbers from list")
+numbers = [1,2,3,4,5,6,7,8]
+for i in numbers:
+    if i % 2 == 0:
+        print(i)
 
         
-# print("Reverse a list")
-# numbers = [1,2,3,4,5]
-# numbers.reverse()
-# print(numbers)
+print("Reverse a list")
+numbers = [1,2,3,4,5]
+numbers.reverse()
+print(numbers)
 
 
-# print("Create student dictionary")
-# student = {
-#     "name":"Vinaya",
-#     "age":20,
-#     "marks":85
-# }
-# print(student)
+print("Create student dictionary")
+student = {
+    "name":"Vinaya",
+    "age":20,
+    "marks":85
+}
+print(student)
 
 
-# print("Update marks")
-# student = {
-#     "name":"Vinaya",
-#     "marks":85
-# }
-# student["marks"] = 95
-# print(student)
+print("Update marks")
+student = {
+    "name":"Vinaya",
+    "marks":85
+}
+student["marks"] = 95
+print(student)
 
 
-# print("Add new key")
-# student = {
-#     "name":"Vinaya",
-#     "marks":95
-# }
-# student["grade"] = "A"
-# print(student)
+print("Add new key")
+student = {
+    "name":"Vinaya",
+    "marks":95
+}
+student["grade"] = "A"
+print(student)
 
 
-# print("Print all keys and values")
-# student = {
-#     "name":"Vinaya",
-#     "age":20,
-#     "marks":90
-# }
-# for key,value in student.items():
-#     print(key,":",value)
+print("Print all keys and values")
+student = {
+    "name":"Vinaya",
+    "age":20,
+    "marks":90
+}
+for key,value in student.items():
+    print(key,":",value)
 
 
-# print("Count number of characters")
-# text = "Hello AI"
-# print(len(text))
+print("Count number of characters")
+text = "Hello AI"
+print(len(text))
 
 
-# print("Reverse a string")
-# text = "Python"
-# print(text[::-1])
+print("Reverse a string")
+text = "Python"
+print(text[::-1])
 
 
-# print("Check palindrome")
-# text = "madam"
-# if text == text[::-1]:
-#     print("Palindrome")
-# else:
-#     print("Not Palindrome")
+print("Check palindrome")
+text = "madam"
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
 
 
-# print("Count vowels")
-# text = "education"
-# count = 0
-# for i in text:
-#     if i in "aeiou":
-#         count += 1
+print("Count vowels")
+text = "education"
+count = 0
+for i in text:
+    if i in "aeiou":
+        count += 1
 
-# print(count)
-
-
-# print("Remove duplicates using set")
-# numbers = [1,2,2,3,4,4,5]
-# print(set(numbers))
+print(count)
 
 
-# print("Find common elements")
-# a = {1,2,3,4}
-# b = {3,4,5,6}
-# print(a.intersection(b))
+print("Remove duplicates using set")
+numbers = [1,2,2,3,4,4,5]
+print(set(numbers))
+
+
+print("Find common elements")
+a = {1,2,3,4}
+b = {3,4,5,6}
+print(a.intersection(b))
 
 
 print("Squares from 1–10")
---
+++ Day4.py
@@ -0,0 +1,89 @@
+# print("Create file and write your details")
+# file = open("details.txt", "w")
+# file.write("Name: Vinaya\n")
+# file.write("Course: BSc Computer Science\n")
+# file.write("Age: 20")
+# file.close()
+
+
+# print("Read file and display")
+# file = open("details.txt", "r")
+# print(file.read())
+# file.close()
+
+# print("Append new data")
+# file = open("details.txt", "a")
+# file.write("\nCollege: St. Paul's College")
+# file.close()
+
+
+# print("Count number of lines")
+# with open("details.txt", "r") as file:
+#     lines = file.readlines()
+#     print(len(lines))
+
+
+# print("Count number of words")
+# with open("details.txt", "r") as file:
+#     text = file.read()
+#     words = text.split()
+#     print(len(words))
+
+# print("Handle divide by zero")
+# try:
+#     num = int(input("Enter number: "))
+#     print(10 / num)
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
+
+
+# print("Handle invalid input")
+# try:
+#     age = int(input("Enter age: "))
+#     print(age)
+# except ValueError:
+#     print("Invalid Input")
+
+
+# print("Use try-except-else-finally")
+# try:
+#     num = int(input("Enter number: "))
+# except:
+#     print("Error")
+# else:
+#     print("No Error")
+# finally:
+#     print("Program Finished")
+
+
+# print("Handle file not found error")
+# try:
+#     file = open("sample.txt", "r")
+#     print(file.read())
+# except FileNotFoundError:
+#     print("File not found")
+
+print("Find longest word in file")
+with open("details.txt", "r") as file:
+    text = file.read()
+    words = text.split()
+    longest = max(words, key=len)
+    print(longest)
+
+
+print("Count vowels in file")
+with open("details.txt", "r") as file:
+    text = file.read().lower()
+    count = 0
+    for i in text:
+        if i in "aeiou":
+            count += 1
+    print(count)
+
+
+print("Remove duplicate lines")
+with open("details.txt", "r") as file:
+    lines = file.readlines()
+unique = set(lines)
+print(unique)
+
