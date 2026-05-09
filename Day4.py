2dd716ac672b13f263ff1d8c58708eedddc4a5ad
Vinaya <vsvinaya26@gmail.com>
2026-05-09 16:08:14 +0530

    Fixed commented code

--- Day4.py
+++ Day4.py
@@ -1,67 +1,67 @@
-# print("Create file and write your details")
-# file = open("details.txt", "w")
-# file.write("Name: Vinaya\n")
-# file.write("Course: BSc Computer Science\n")
-# file.write("Age: 20")
-# file.close()
+print("Create file and write your details")
+file = open("details.txt", "w")
+file.write("Name: Vinaya\n")
+file.write("Course: BSc Computer Science\n")
+file.write("Age: 20")
+file.close()
 
 
-# print("Read file and display")
-# file = open("details.txt", "r")
-# print(file.read())
-# file.close()
+print("Read file and display")
+file = open("details.txt", "r")
+print(file.read())
+file.close()
 
-# print("Append new data")
-# file = open("details.txt", "a")
-# file.write("\nCollege: St. Paul's College")
-# file.close()
-
-
-# print("Count number of lines")
-# with open("details.txt", "r") as file:
-#     lines = file.readlines()
-#     print(len(lines))
+print("Append new data")
+file = open("details.txt", "a")
+file.write("\nCollege: St. Paul's College")
+file.close()
 
 
-# print("Count number of words")
-# with open("details.txt", "r") as file:
-#     text = file.read()
-#     words = text.split()
-#     print(len(words))
-
-# print("Handle divide by zero")
-# try:
-#     num = int(input("Enter number: "))
-#     print(10 / num)
-# except ZeroDivisionError:
-#     print("Cannot divide by zero")
+print("Count number of lines")
+with open("details.txt", "r") as file:
+    lines = file.readlines()
+    print(len(lines))
 
 
-# print("Handle invalid input")
-# try:
-#     age = int(input("Enter age: "))
-#     print(age)
-# except ValueError:
-#     print("Invalid Input")
+print("Count number of words")
+with open("details.txt", "r") as file:
+    text = file.read()
+    words = text.split()
+    print(len(words))
+
+print("Handle divide by zero")
+try:
+    num = int(input("Enter number: "))
+    print(10 / num)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
 
 
-# print("Use try-except-else-finally")
-# try:
-#     num = int(input("Enter number: "))
-# except:
-#     print("Error")
-# else:
-#     print("No Error")
-# finally:
-#     print("Program Finished")
+print("Handle invalid input")
+try:
+    age = int(input("Enter age: "))
+    print(age)
+except ValueError:
+    print("Invalid Input")
 
 
-# print("Handle file not found error")
-# try:
-#     file = open("sample.txt", "r")
-#     print(file.read())
-# except FileNotFoundError:
-#     print("File not found")
+print("Use try-except-else-finally")
+try:
+    num = int(input("Enter number: "))
+except:
+    print("Error")
+else:
+    print("No Error")
+finally:
+    print("Program Finished")
+
+
+print("Handle file not found error")
+try:
+    file = open("sample.txt", "r")
+    print(file.read())
+except FileNotFoundError:
+    print("File not found")
 
 print("Find longest word in file")
 with open("details.txt", "r") as file:
