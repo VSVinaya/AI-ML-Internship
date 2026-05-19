d3b5fcb7645dfdb352bd2cfd64440b97903739cc
Vinaya <vsvinaya26@gmail.com>
2026-05-19 12:11:07 +0530

    OOPs Concept Day 10 completed

--- Day10.py
+++ Day10.py
@@ -1,66 +1,66 @@
-# #Create class Create object and print values
-# class Car:
-#     name = ""
-#     price = 0
-# c1 = Car()
-# c1.name = "BMW"
-# c1.price = 5000000
-# print(c1.name)
-# print(c1.price)
+#Create class Create object and print values
+class Car:
+    name = ""
+    price = 0
+c1 = Car()
+c1.name = "BMW"
+c1.price = 5000000
+print(c1.name)
+print(c1.price)
 
 
-# #Create class Student using constructor
-# class Student:
-#     def __init__(self, name, marks):
-#         self.name = name
-#         self.marks = marks
-# s1 = Student("John", 85)
-# print(s1.name)
-# print(s1.marks)
+#Create class Student using constructor
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+s1 = Student("John", 85)
+print(s1.name)
+print(s1.marks)
 
 
-# print("Check pass/fail and calculate grade")
-# class Student:
-#     def __init__(self, name, marks):
-#         self.name = name
-#         self.marks = marks
-#     def is_pass(self):
-#         if self.marks >= 50:
-#             return "Pass"
-#         else:
-#             return "Fail"
-#     def grade(self):
-#         if self.marks >= 90:
-#             return "A"
-#         elif self.marks >= 75:
-#             return "B"
-#         else:
-#             return "C"
-# s1 = Student("Sara", 92)
-# print(s1.is_pass())
-# print(s1.grade())
+print("Check pass/fail and calculate grade")
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    def is_pass(self):
+        if self.marks >= 50:
+            return "Pass"
+        else:
+            return "Fail"
+    def grade(self):
+        if self.marks >= 90:
+            return "A"
+        elif self.marks >= 75:
+            return "B"
+        else:
+            return "C"
+s1 = Student("Sara", 92)
+print(s1.is_pass())
+print(s1.grade())
 
 
-# print("Create list of students and find topper")
-# class Student:
-#     def __init__(self, name, marks):
-#         self.name = name
-#         self.marks = marks
-# students = [
-#     Student("A",70),
-#     Student("B",95),
-#     Student("C",60)
-# ]
-# for s in students:
-#     print(s.name, s.marks)
-# topper = students[0]
-# for s in students:
-#     if s.marks > topper.marks:
-#         topper = s
-# print("Topper:", topper.name)
+print("Create list of students and Find topper")
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+students = [
+    Student("A",70),
+    Student("B",95),
+    Student("C",60)
+]
+for s in students:
+    print(s.name, s.marks)
+topper = students[0]
+for s in students:
+    if s.marks > topper.marks:
+        topper = s
+print("Topper:", topper.name)
 
 
-print("Create Class BankAccount")
+#Create Class BankAccount
 class BankAccount:
     def __init__(self, balance):
         self.balance = balance
@@ -76,7 +76,7 @@ b1.withdraw(200)
 b1.check_balance()
 
 
-print("Create Class Product")
+#Create Class Product
 class Product:
     def __init__(self, name, price):
         self.name = name
