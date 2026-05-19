23e93300bb0d8dcbf47167a7b2b02eea110fbb85
Vinaya <vsvinaya26@gmail.com>
2026-05-19 08:22:13 +0530

    Day 9 data processing completed

--- Day8.py
+++ Day8.py
@@ -1,98 +1,98 @@
-# print("Function to find factorial")
-# def factorial(n):
-#     fact = 1
-#     for i in range(1, n+1):
-#         fact *= i
-#     print(fact)
-# factorial(5)
+print("Function to find factorial")
+def factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
+factorial(5)
 
 
-# print("Function to check prime number")
-# def prime(n):
-#     count = 0
-#     for i in range(1, n+1):
-#         if n % i == 0:
-#             count += 1
-#     if count == 2:
-#         print("Prime Number")
-#     else:
-#         print("Not Prime Number")
+print("Function to check prime number")
+def prime(n):
+    count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        print("Prime Number")
+    else:
+        print("Not Prime Number")
 
-# prime(7)
-
-
-# print("Function to reverse string")
-# def reverse(text):
-#     print(text[::-1])
-# reverse("python")
+prime(7)
 
 
-# print("Function to calculate average")
-# def average(a,b,c):
-#     avg = (a+b+c)/3
-#     print(avg)
-# average(10,20,30)
+print("Function to reverse string")
+def reverse(text):
+    print(text[::-1])
+reverse("python")
 
 
-# print("Factorial using recursion")
-# def factorial(n):
-#     if n == 1:
-#         return 1
-#     return n * factorial(n-1)
-# print(factorial(5))
+print("Function to calculate average")
+def average(a,b,c):
+    avg = (a+b+c)/3
+    print(avg)
+average(10,20,30)
 
 
-# print("Sum of numbers using recursion")
-# def total(n):
-#     if n == 1:
-#         return 1
-#     return n + total(n-1)
-# print(total(5))
+print("Factorial using recursion")
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+print(factorial(5))
 
 
-# print("Fibonacci series")
-# def fibonacci(n):
-#     if n <= 1:
-#         return n
-#     return fibonacci(n-1) + fibonacci(n-2)
-# for i in range(6):
-#     print(fibonacci(i))
+print("Sum of numbers using recursion")
+def total(n):
+    if n == 1:
+        return 1
+    return n + total(n-1)
+print(total(5))
 
 
-# print("Square all numbers")
-# nums = [1,2,3,4]
-# result = list(map(lambda x:x*x, nums))
-# print(result)
+print("Fibonacci series")
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+for i in range(6):
+    print(fibonacci(i))
 
 
-# print("Double all values")
-# nums = [1,2,3]
-# result = list(map(lambda x:x*2, nums))
-# print(result)
+print("Square all numbers")
+nums = [1,2,3,4]
+result = list(map(lambda x:x*x, nums))
+print(result)
 
 
-# print("Convert list to uppercase")
-# words = ["ai","ml","python"]
-# result = list(map(lambda x:x.upper(), words))
-# print(result)
+print("Double all values")
+nums = [1,2,3]
+result = list(map(lambda x:x*2, nums))
+print(result)
 
 
-# print("Find even numbers")
-# nums = [1,2,3,4,5,6]
-# result = list(filter(lambda x:x%2==0, nums))
-# print(result)
+print("Convert list to uppercase")
+words = ["ai","ml","python"]
+result = list(map(lambda x:x.upper(), words))
+print(result)
 
 
-# print("Filter students with marks > 50")
-# marks = [30,60,80,45]
-# result = list(filter(lambda x:x>50, marks))
-# print(result)
+print("Find even numbers")
+nums = [1,2,3,4,5,6]
+result = list(filter(lambda x:x%2==0, nums))
+print(result)
 
 
-# print("Filter words with length > 5")
-# words = ["python","ai","machine","data"]
-# result = list(filter(lambda x:len(x)>5, words))
-# print(result)
+print("Filter students with marks > 50")
+marks = [30,60,80,45]
+result = list(filter(lambda x:x>50, marks))
+print(result)
+
+
+print("Filter words with length > 5")
+words = ["python","ai","machine","data"]
+result = list(filter(lambda x:len(x)>5, words))
+print(result)
 
 
 print("Sum of digits using recursion")
---
+++ Day9.py
@@ -0,0 +1,151 @@
+# #Create file with student data
+# file = open("students.txt","w")
+# file.write("John,80\n")
+# file.write("Sara,90\n")
+# file.write("Alex,70")
+# file.close()
+
+
+# print("Read and store in list of dictionary")
+# students = []
+# with open("students.txt","r") as file:
+#     for line in file:
+#         name, marks = line.strip().split(",")
+#         students.append({
+#             "name":name,
+#             "marks":int(marks)
+#         })
+# print(students)
+
+
+# print("Print all students")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90}
+# ]
+# for s in students:
+#     print(s)
+
+
+# print("Find average marks")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90}
+# ]
+# total = sum(s["marks"] for s in students)
+# avg = total / len(students)
+# print(avg)
+
+
+# print("Find topper")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90}
+# ]
+# top = max(students, key=lambda x:x["marks"])
+# print(top)
+
+
+# print("students with marks > 75")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90},
+#     {"name":"Alex","marks":70}
+# ]
+# for s in students:
+#     if s["marks"] > 75:
+#         print(s)
+
+
+# print("Number of students")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90}
+# ]
+# print(len(students))
+
+
+# print("Lowest marks")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90},
+#     {"name":"Alex","marks":70}
+# ]
+# low = min(students, key=lambda x:x["marks"])
+# print(low)
+
+
+# print("Sort students by marks")
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90},
+#     {"name":"Alex","marks":70}
+# ]
+# sorted_students = sorted(students, key=lambda x:x["marks"])
+# print(sorted_students)
+
+
+# print("Function to Calculate average")
+# def average(students):
+#     total = sum(s["marks"] for s in students)
+#     return total / len(students)
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90}
+# ]
+# print(average(students))
+
+
+# print("Function to find topper")
+# def topper(students):
+#     return max(students, key=lambda x:x["marks"])
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Sara","marks":90}
+# ]
+# print(topper(students))
+
+
+# print("Function to filter students")
+# def filter_students(students):
+#     result = []
+#     for s in students:
+#         if s["marks"] > 75:
+#             result.append(s)
+#     return result
+# students = [
+#     {"name":"John","marks":80},
+#     {"name":"Alex","marks":70}
+# ]
+# print(filter_students(students))
+
+
+print("Student with second highest marks")
+students = [
+    {"name":"John","marks":80},
+    {"name":"Sara","marks":90},
+    {"name":"Alex","marks":70}]
+students.sort(key=lambda x:x["marks"])
+print(students[-2])
+
+
+print("Count passed and failed students")
+marks = [80,40,90,30]
+passed = 0
+failed = 0
+for m in marks:
+    if m >= 50:
+        passed += 1
+    else:
+        failed += 1
+print("Passed:", passed)
+print("Failed:", failed)
+
+
+print("Convert data into dictionary format")
+names = ["John","Sara"]
+marks = [80,90]
+students = {}
+for i in range(len(names)):
+    students[names[i]] = marks[i]
+print(students)
