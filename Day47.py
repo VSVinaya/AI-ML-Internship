012851a9f6ee27072d7e69d62e781e1246b338bd
Vinaya <vsvinaya26@gmail.com>
2026-07-16 23:03:37 +0530

    Day48 custom object detection completed

--- Day47.py
+++ Day47.py
@@ -1,19 +1,19 @@
-# print("Run Detection on a Video File")
-# import cv2
-# from ultralytics import YOLO
-# model = YOLO("yolov8n.pt")
-# video = cv2.VideoCapture("dog.mp4.mp4")
-# while True:
-#     success, frame = video.read()
-#     if not success:
-#         break
-#     results = model(frame)
-#     annotated_frame = results[0].plot()
-#     cv2.imshow("YOLO Video Detection", annotated_frame)
-#     if cv2.waitKey(1) == 27:
-#         break
-# video.release()
-# cv2.destroyAllWindows()
+print("Run Detection on a Video File")
+import cv2
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt")
+video = cv2.VideoCapture("dog.mp4.mp4")
+while True:
+    success, frame = video.read()
+    if not success:
+        break
+    results = model(frame)
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLO Video Detection", annotated_frame)
+    if cv2.waitKey(1) == 27:
+        break
+video.release()
+cv2.destroyAllWindows()
  
 
 print("Run Real-Time Webcam Detection")
