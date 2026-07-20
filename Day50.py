23fd87be6ec4aa5899095f4511c0fb1f1ff6e282
Vinaya <vsvinaya26@gmail.com>
2026-07-20 22:00:22 +0530

    Day 50 complete yolo helmet detection project

---
+++ Day50.py
@@ -0,0 +1,21 @@
+print("Real-Time Helmet Detection")
+import cv2 
+from ultralytics import YOLO 
+model = YOLO( 
+    "runs/detect/train/weights/best.pt" 
+    ) 
+cap = cv2.VideoCapture(0) 
+while True: 
+    success, frame = cap.read() 
+    if not success: 
+        break 
+    results = model(frame) 
+    annotated_frame = results[0].plot() 
+    cv2.imshow( 
+        "Helmet Detection", 
+        annotated_frame 
+        ) 
+    if cv2.waitKey(1) == 27: 
+        break 
+    cap.release() 
+    cv2.destroyAllWindows()
